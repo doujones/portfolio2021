f4a6643c1002250818c4fc6d40ddf80a121539d7
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2021-04-04 18:18:58 -0400

    third commit

---
+++ public/data.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const links = [
+    {
+        id: 1,
+        url: "/",
+        text: "home"       
+    },
+    {
+        id: 2,
+        url: "/about",
+        text: "about"       
+    },
+    {
+        id: 3,
+        url: "/projects",
+        text: "projects"       
+    },
+    {
+        id: 4,
+        url: "/biography",
+        text: "biography"       
+    },
+]
